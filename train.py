39ac1f34b4c16b3c96e1908d1d9b9869180a3c8b
Nima Shoghi <nimashoghi@gmail.com>
2020-08-02 23:43:59 +0000

    Added new data

--- train.py
+++ train.py
@@ -13,43 +13,52 @@ from network import MobileNetV2
 from util import test
 
 
-
 if __name__ == "__main__":
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ['CUDA_VISIBLE_DEVICES'] = '2,4'
-
     start_time = time.time()
     max_val_acc = 0
 
     # load data
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding = 4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
+    transform_test = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
 
-    trainset = CIFAR10("/home/tinyalpha/dataset/cifar10", transform = transform_train, train = True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size = 128, shuffle = True, num_workers = 2)
+    trainset = CIFAR10(
+        "./cifar10-train", transform=transform_train, train=True, download=True,
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=1
+    )
 
-    testset = CIFAR10("/home/tinyalpha/dataset/cifar10", transform = transform_test, train = False)
-    testloader = torch.utils.data.DataLoader(testset, batch_size = 64, shuffle = False, num_workers = 2)
+    testset = CIFAR10(
+        "./cifar10-test", transform=transform_test, train=False, download=True,
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=64, shuffle=False, num_workers=1
+    )
 
     # write header
-    with open('log.csv', 'w') as f:
+    with open("log.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerow(["iteration", "train_loss", "val_loss", "acc", "val_acc"])
 
-    device_ids = [0,1]
+    device_ids = [0, 1]
     # build model and optimizer
-    model = nn.DataParallel(MobileNetV2(10, alpha = 1), device_ids = device_ids)
-    # model = MobileNetV2(10, alpha = 1)
+    # model = nn.DataParallel(MobileNetV2(10, alpha=1), device_ids=device_ids)
+    model = MobileNetV2(10, alpha=1)
     model.cuda()
     criterion = nn.CrossEntropyLoss()
     # model.load_state_dict(torch.load("weights.pkl"))
-
 
     # train
     i = 0
@@ -61,34 +70,40 @@ if __name__ == "__main__":
 
         # update lr
         if epoch == 0:
-            optimizer = optim.SGD(model.parameters(), lr = 1e-1, weight_decay = 4e-5, momentum = 0.9)
+            optimizer = optim.SGD(
+                model.parameters(), lr=1e-1, weight_decay=4e-5, momentum=0.9
+            )
         elif epoch == 150:
-            optimizer = optim.SGD(model.parameters(), lr = 1e-2, weight_decay = 4e-5, momentum = 0.9)
+            optimizer = optim.SGD(
+                model.parameters(), lr=1e-2, weight_decay=4e-5, momentum=0.9
+            )
         elif epoch == 225:
-            optimizer = optim.SGD(model.parameters(), lr = 1e-3, weight_decay = 4e-5, momentum = 0.9)
+            optimizer = optim.SGD(
+                model.parameters(), lr=1e-3, weight_decay=4e-5, momentum=0.9
+            )
 
         # iteration over all train data
         for data in trainloader:
             # shift to train mode
             model.train()
-            
+
             # get the inputs
             inputs, labels = data
             inputs = inputs.cuda()
             labels = labels.cuda()
-            
+
             # forward + backward + optimize
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             # count acc,loss on trainset
             _, predicted = torch.max(outputs.data, 1)
-            
+
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()        
+            correct += (predicted == labels).sum().item()
             train_loss += loss.item()
             counter += 1
 
@@ -96,27 +111,29 @@ if __name__ == "__main__":
                 # get acc,loss on trainset
                 acc = correct / total
                 train_loss /= counter
-                
+
                 # test
                 val_loss, val_acc = test(model, testloader, criterion)
 
-                print('iteration %d , epoch %d:  loss: %.4f  val_loss: %.4f  acc: %.4f  val_acc: %.4f' 
-                      %(i, epoch, train_loss, val_loss, acc, val_acc))
-                
+                print(
+                    "iteration %d , epoch %d:  loss: %.4f  val_loss: %.4f  acc: %.4f  val_acc: %.4f"
+                    % (i, epoch, train_loss, val_loss, acc, val_acc)
+                )
+
                 # save logs and weights
-                with open('log.csv', 'a') as f:
+                with open("log.csv", "a") as f:
                     writer = csv.writer(f)
                     writer.writerow([i, train_loss, val_loss, acc, val_acc])
                 if val_acc > max_val_acc:
-                    torch.save(model.state_dict(), 'weights.pkl')
+                    torch.save(model.state_dict(), "weights.pkl")
                     max_val_acc = val_acc
-                    
+
                 # reset counters
                 correct, total = 0, 0
                 train_loss, counter = 0, 0
 
             i += 1
-        print("epoch time %.4f min" %((time.time() - epoch_start_time)/60))
+        print("epoch time %.4f min" % ((time.time() - epoch_start_time) / 60))
 
     end_time = time.time()
-    print("total time %.1f h" %((end_time - start_time)/3600))
+    print("total time %.1f h" % ((end_time - start_time) / 3600))
